f12a33f6a5e56811ccf339a4497a5595b03280c1
wish <chuenreang@naver.com>
2025-01-09 15:17:21 +0900

    [Feat] register, login(구현중)

--- src/main/java/com/shinsegae/project/member/controller/MemberController.java
+++ src/main/java/com/shinsegae/project/member/controller/MemberController.java
@@ -1,9 +1,12 @@
 package com.shinsegae.project.member.controller;
 
 import com.shinsegae.project.member.service.UserService;
+import com.shinsegae.project.member.vo.MemberVO;
 import com.shinsegae.project.member.vo.UserVO;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +20,28 @@ public class MemberController {
 
     @GetMapping("login")
     public String login() {
-        System.out.println("========================login========================");
+        System.out.println("login");
         return "user/member/login";
+    }
+
+    @PostMapping("login") //contextpath/member/login
+    public String login(UserVO userVO, HttpSession session, Model model) {
+        System.out.println("UserVO" + userVO);
+        boolean result = userService.login(userVO); //싱글톤 객체 찾아서 메서드 호출해야할 것 같음
+        if (result) {
+            session.setAttribute("id", userVO.getId());
+        } else {
+            model.addAttribute("result", "로그인에 실패하였습니다!");
+        }
+        System.out.println("세션값 설정 확인>> " + session.getAttribute("id"));
+        return "/user/home";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpSession session) {
+        session.removeAttribute("id");
+        System.out.println("logout");
+        return "/index";
     }
 
     @GetMapping("register")
@@ -28,8 +51,10 @@ public class MemberController {
 
     @PostMapping("register")
     public String register2(UserVO userVO) {
+        System.out.println("userVO >> " + userVO);
         userService.insertUser(userVO);
         return "user/member/login";
+
     }
 
 
--- src/main/java/com/shinsegae/project/member/service/UserService.java
+++ src/main/java/com/shinsegae/project/member/service/UserService.java
@@ -3,6 +3,7 @@ package com.shinsegae.project.member.service;
 import com.shinsegae.project.member.mapper.UserMapper;
 import com.shinsegae.project.member.vo.UserVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -10,10 +11,29 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     public int insertUser(UserVO userVO) {
-        return userMapper.insertUser(userVO);
+        String pw2 = passwordEncoder.encode(userVO.getPassword());
+        userVO.setPassword(pw2);
+        System.out.println("vo에 암호화된 pw >>> " + userVO.getPassword());
+        int result = userMapper.insertUser(userVO);
+        return result;
+    };
+
+    public boolean login(UserVO userVO) {
+        UserVO userVO1 = userMapper.selectUserById(userVO.getId());
+        if (userVO1 != null) {
+            if (passwordEncoder.matches(userVO.getPassword(), userVO1.getPassword())) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
+
     public int updateUser(UserVO userVO) {
         return userMapper.updateUser(userVO);
     };
@@ -26,4 +46,7 @@ public class UserService {
     public String selectIdByUserTel(String tel){
         return userMapper.selectIdByUserTel(tel);
     };
+    public boolean checkId(String id) {
+        return userMapper.selectUserById(id) == null;
+    }
 }
