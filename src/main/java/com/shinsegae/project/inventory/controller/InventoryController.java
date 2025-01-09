1987c0255a8f960faf006fb99a15d7a7ed29e02c
ParkHeonWoo <phwoo1315@naver.com>
2025-01-09 18:13:10 +0900

    feat: ui 일부 구현 및 단가, 재고량 로직 추가
    Mapper.xml에 sql문 추가
    InventoryService.java 파일에 해쉬맵을 사용해 총합 구하는 로직 추가
    뷰에 전달할 InventoryController.java 구현
    어노테이션을 사용하여 DTO파일 변경
    재고 관리화면과 재고 수정화면 UI 일부 구현

--- src/main/java/com/shinsegae/project/inventory/controller/InventoryController.java
+++ src/main/java/com/shinsegae/project/inventory/controller/InventoryController.java
@@ -1,15 +1,32 @@
 package com.shinsegae.project.inventory.controller;
 
+import com.shinsegae.project.inventory.service.InventoryService;
+import com.shinsegae.project.inventory.vo.InventoryManagementDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Map;
+
+
 @Controller
 @RequestMapping("admin/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+    private final InventoryService inventoryService;
 
     @GetMapping("confirm_inventory_list")
-    public String confirm_inventory_list() {
+    public String confirm_inventory_list(Model model) {
+
+        List<InventoryManagementDTO> list = inventoryService.selectInventoryAll();
+        Map<String, Integer> sumList = inventoryService.getSum(list);
+        model.addAttribute("confirm_inventory_list", inventoryService.selectInventoryAll());
+        model.addAttribute("sumList", sumList);
+        //모델로 전달(sumList)
+        //${sumList.sum1} 출력
         return "admin/inventory/confirm_inventory_list";
     }
 
--- src/main/java/com/shinsegae/project/inventory/service/InventoryService.java
+++ src/main/java/com/shinsegae/project/inventory/service/InventoryService.java
@@ -6,7 +6,10 @@ import com.shinsegae.project.inventory.vo.InventoryVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,26 @@ public class InventoryService {
         return inventoryMapper.selectInventoryAll();
     }
 
+    public Map<String, Integer> getSum(List<InventoryManagementDTO> list){
+        Map<String, Integer> map = new HashMap<>(); // 키 - 값 쌍으로 데이터 저장 인터페이스
+
+        int sum1 = 0; //900
+        int sum2 = 0; //1000
+
+        for (InventoryManagementDTO dto : list) {
+            sum1 += dto.getPrice();
+        }
+
+        map.put("sum1", sum1);
+
+        for (InventoryManagementDTO dto : list) {
+            sum2 += dto.getStock();
+        }
+
+        map.put("sum2", sum2);
+
+        return map; //{900, 1000}
+    }
     public List<InventoryManagementDTO> getInventoryByContent() {
         return inventoryMapper.getInventoryByContent();
     }
