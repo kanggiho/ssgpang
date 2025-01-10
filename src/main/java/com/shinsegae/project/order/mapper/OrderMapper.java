1d19c372f288f2f8dba5b5b64ecec4b5d68cc707
kanggiho <kgh5554@naver.com>
2025-01-10 15:55:52 +0900

    발주기능완성

--- src/main/java/com/shinsegae/project/order/mapper/OrderMapper.java
+++ src/main/java/com/shinsegae/project/order/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.shinsegae.project.order.mapper;
 
 import com.shinsegae.project.order.vo.OrderInventoryManagementDTO;
+import com.shinsegae.project.order.vo.OrderInventoryUpdateVO;
 import com.shinsegae.project.order.vo.OrderManagementDTO;
 import com.shinsegae.project.order.vo.OutputVO;
 import org.apache.ibatis.annotations.Mapper;
@@ -13,6 +14,8 @@ public interface OrderMapper {
     // 발주하기
     List<OrderInventoryManagementDTO> selectOrderInventoryManagementAll();
     void insertOutput(OutputVO outputVO);
+
+    void updateInventory(OrderInventoryUpdateVO updateVO);
 
 
     // 발주내역확인
--- src/main/java/com/shinsegae/project/order/service/OrderService.java
+++ src/main/java/com/shinsegae/project/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.shinsegae.project.order.service;
 
 import com.shinsegae.project.order.vo.OrderInventoryManagementDTO;
+import com.shinsegae.project.order.vo.OrderInventoryUpdateVO;
 import com.shinsegae.project.order.vo.OrderManagementDTO;
 import com.shinsegae.project.order.vo.OutputVO;
 import com.shinsegae.project.order.mapper.OrderMapper;
@@ -19,7 +20,11 @@ public class OrderService {
     public List<OrderInventoryManagementDTO> selectOutputTable(){ return orderMapper.selectOrderInventoryManagementAll();}
 
     public void saveOutput(OutputVO outputVO) {
+        OrderInventoryUpdateVO orderInventoryUpdateVO = new OrderInventoryUpdateVO();
+        orderInventoryUpdateVO.setChange(outputVO.getRelease_quantity());
+        orderInventoryUpdateVO.setProductCode(outputVO.getProduct_code());
         orderMapper.insertOutput(outputVO);
+        orderMapper.updateInventory(orderInventoryUpdateVO);
     }
 
 
