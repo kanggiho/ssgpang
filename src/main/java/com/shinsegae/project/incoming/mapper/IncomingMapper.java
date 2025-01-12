2794cc828a3a9fec30cec2cde8060ef0b6b8f2bf
sangwooLee1231 <tkdguq0654@naver.com>
2025-01-12 22:37:21 +0900

    입고 관리 구현 완료

--- src/main/java/com/shinsegae/project/incoming/mapper/IncomingMapper.java
+++ src/main/java/com/shinsegae/project/incoming/mapper/IncomingMapper.java
@@ -1,11 +1,17 @@
 package com.shinsegae.project.incoming.mapper;
 
-import com.shinsegae.project.incoming.vo.InputVO;
+import com.shinsegae.project.incoming.vo.IncomingInputVO;
+import com.shinsegae.project.incoming.vo.IncomingManagementDTO;
+import com.shinsegae.project.incoming.vo.IncomingRequestDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface IncomingMapper {
-    List<InputVO> selectInputAll();
+    List<IncomingManagementDTO> selectInputAll();
+    List<IncomingRequestDTO> selectRequestAll();
+    int insertIncomingRequest(IncomingInputVO incomingInputVO);
+
+
 }
